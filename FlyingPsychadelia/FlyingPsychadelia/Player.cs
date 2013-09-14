d69686f585894c3c877c6e8cf845615644fe9a78
SingleCopy <rudd.andy@gmail.com>
2013-09-14 13:18:59 +0100

    Fixed merge conflict in player

--- FlyingPsychadelia/FlyingPsychadelia/Player.cs
+++ FlyingPsychadelia/FlyingPsychadelia/Player.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
 using FuncWorks.XNA.XTiled;
 using Microsoft.Xna.Framework.Graphics;
@@ -13,12 +15,26 @@ namespace FlyingPsychadelia
         private Rectangle _Rectangle;
         private Texture2D _Texture;
         public Vector2 Velocity;
+        private Body _physicsBody;
+
+        public Player(Texture2D texture, int x, int y, World world)
+        {
+            _Rectangle = new Rectangle(x, y, texture.Width, texture.Height);
+            _Texture = texture;
+
+            //We create a body object and make it dynamic (movable)
+
+
+            _physicsBody = BodyFactory.CreateRectangle(world, _Rectangle.Width, _Rectangle.Height, 1.0f);
+            _physicsBody.BodyType = BodyType.Dynamic;
+
+        }
         public void MoveLeft()
         {
-            _Rectangle.X  -= 10;
+            _Rectangle.X -= 10;
         }
-                                     
-                                     
+
+
         public Rectangle Bounds
         {
             get
@@ -26,14 +42,11 @@ namespace FlyingPsychadelia
                 return _Rectangle;
             }
         }
-        public Player(Texture2D texture, int X, int Y)
-        {
-            _Rectangle = new Rectangle(X,Y,texture.Width, texture.Height);
-            _Texture = texture;
-        }
+
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_Texture,Bounds, Color.White);
+            spriteBatch.Draw(_Texture, _physicsBody.Position, Color.White);
+
         }
         public void MoveRight()
         {
