0136e8faff9bebbbfc7887394c1e665a21d9465f
SingleCopy <rudd.andy@gmail.com>
2013-09-14 16:09:17 +0100

    Farseer physics added

--- FlyingPsychadelia/FlyingPsychadelia/Game1.cs
+++ FlyingPsychadelia/FlyingPsychadelia/Game1.cs
@@ -1,6 +1,11 @@
 ﻿#region Using Statements
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using FarseerPhysics;
+using FarseerPhysics.DebugView;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
 using FuncWorks.XNA.XTiled;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
@@ -21,11 +26,63 @@ namespace FlyingPsychadelia
         SpriteBatch spriteBatch;
         private Map _map;
         private Player _player;
+        private World _world;
+        private Body _groundBody;
+        private Texture2D _base;
+        private DebugViewXNA _debugView;
+        private MapObject _groundCollision;
+
         public Game1()
             : base()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            _world = new World(new Vector2(0f, 1f));
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Map.InitObjectDrawing(GraphicsDevice);
+            _map = Content.Load<Map>("map1");
+
+            Texture2D _PlayerTexture = Content.Load<Texture2D>("Player.png");
+            _player = new Player(_PlayerTexture, 0, 0, _world);
+
+            ConvertUnits.SetDisplayUnitToSimUnitRatio(100f);
+            _debugView = new DebugViewXNA(_world);
+
+            _debugView.RemoveFlags(DebugViewFlags.Controllers);
+            _debugView.RemoveFlags(DebugViewFlags.Joint);
+
+            _debugView.LoadContent(GraphicsDevice, Content);
+
+
+            var collisionLayer = _map.ObjectLayers["GroundCollision"];
+            _groundCollision = collisionLayer.MapObjects.Single();
+
+            _groundBody = BodyFactory.CreateRectangle(_world, ConvertUnits.ToSimUnits(_groundCollision.Bounds.Width), ConvertUnits.ToSimUnits(_groundCollision.Bounds.Height), 1f);
+            _groundBody.BodyType = BodyType.Static;
+            _groundBody.Position = new Vector2(ConvertUnits.ToSimUnits(_groundCollision.Bounds.X), ConvertUnits.ToSimUnits(_groundCollision.Bounds.Y));
+
+            _groundCollision.Bounds = new Rectangle(0, _groundCollision.Bounds.Height,
+                                                    _groundCollision.Bounds.Height, _groundCollision.Bounds.Width);
+
+            //_groundBody = BodyFactory.CreateRectangle(_world, ConvertUnits.ToSimUnits(150), ConvertUnits.ToSimUnits(50), 10f);
+            //_groundBody.Position = ConvertUnits.ToSimUnits(0, 250);
+            //_groundBody.IsStatic = true;
+            //_groundBody.Restitution = 0.2f;
+            //_groundBody.Friction = 0.2f;
+
+            //var body = BodyFactory.CreateRectangle(_world, ConvertUnits.ToSimUnits(260), ConvertUnits.ToSimUnits(50), 1f);
+            //body.BodyType = BodyType.Static;
+            //body.Position = ConvertUnits.ToSimUnits(new Vector2(0, 160));
         }
 
         /// <summary>
@@ -39,21 +96,6 @@ namespace FlyingPsychadelia
             // TODO: Add your initialization logic here
 
             base.Initialize();
-        }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
-        protected override void LoadContent()
-        {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            Map.InitObjectDrawing(GraphicsDevice);
-            _map = Content.Load<Map>("map1");
-            Texture2D _PlayerTexture = Content.Load<Texture2D>("Player.png");
-            _player = new Player(_PlayerTexture, 0, 0);
-            // TODO: use this.Content to load your game content here
         }
 
         /// <summary>
@@ -75,32 +117,50 @@ namespace FlyingPsychadelia
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-            if (Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                _player.MoveRight();
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
-            {
-                _player.MoveLeft();
-            }
+            _player.Update(gameTime);
 
+            _world.Step(Math.Min((float)gameTime.ElapsedGameTime.TotalSeconds, (1f / 30f)));
             base.Update(gameTime);
         }
 
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /// <param name="gameTime">Provides a snapshort of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
 
-            _map.Draw(spriteBatch, new Rectangle(0, 0, 320, 320));
-            _player.Draw(spriteBatch);
+            //spriteBatch.Draw(_map.Tilesets[0].Texture, new Rectangle(0, 0, 320, 320), Color.White);
+            //_map.Draw(spriteBatch, ConvertUnits.ToDisplayUnits(_groundBody.Position));
 
+            //_map.Draw(spriteBatch, new Rectangle(0, 0, (int) ConvertUnits.ToDisplayUnits(_groundBody.Position.X), (int) ConvertUnits.ToDisplayUnits(_groundBody.Position.Y)));
+
+            //spriteBatch.Draw(_map.Tilesets[0].Texture, ConvertUnits.ToDisplayUnits(_groundBody.Position), Color.White);
+
+            var destination = new Rectangle
+               (
+                   (int)_groundBody.Position.X,
+                   (int)_groundBody.Position.Y,
+                   (int)_groundCollision.Bounds.X,
+                   (int)_groundCollision.Bounds.Y
+               );
+
+            _map.Draw(spriteBatch, _groundCollision.Bounds);
+
+            //for (int i = 0; i < _map.ObjectLayers.Count; i++)
+            //{
+            //    _map.DrawObjectLayer(spriteBatch, i, new Rectangle(0, 0, 320, 320), i);
+            //}
+            _player.Draw(spriteBatch);
+            var projection = Matrix.CreateOrthographicOffCenter(
+                0f,
+                ConvertUnits.ToSimUnits(GraphicsDevice.Viewport.Width),
+                ConvertUnits.ToSimUnits(GraphicsDevice.Viewport.Height), 0f, 0f,
+                1f);
+            _debugView.RenderDebugData(ref projection);
             spriteBatch.End();
 
             base.Draw(gameTime);
--- FlyingPsychadelia/FlyingPsychadelia/Player.cs
+++ FlyingPsychadelia/FlyingPsychadelia/Player.cs
@@ -2,55 +2,60 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FarseerPhysics;
 using FarseerPhysics.Dynamics;
 using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
 using FuncWorks.XNA.XTiled;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace FlyingPsychadelia
 {
     public class Player
     {
-        private Rectangle _Rectangle;
         private Texture2D _Texture;
         public Vector2 Velocity;
-        private Body _physicsBody;
+        private Body _body;
 
         public Player(Texture2D texture, int x, int y, World world)
         {
-            _Rectangle = new Rectangle(x, y, texture.Width, texture.Height);
             _Texture = texture;
 
             //We create a body object and make it dynamic (movable)
 
 
-            _physicsBody = BodyFactory.CreateRectangle(world, _Rectangle.Width, _Rectangle.Height, 1.0f);
-            _physicsBody.BodyType = BodyType.Dynamic;
+            _body = BodyFactory.CreateRectangle(world, ConvertUnits.ToSimUnits(texture.Width), ConvertUnits.ToSimUnits(texture.Height), 10.0f);
+            _body.BodyType = BodyType.Dynamic;
+            _body.Position = ConvertUnits.ToSimUnits(new Vector2(30, 0));
+            _body.SleepingAllowed = false;
+            _body.Restitution = .7f;
+            _body.Friction = .2f;
+
+            _body.Mass = 1;
+            //_physicsBody.LocalCenter = new Vector2(ConvertUnits.ToSimUnits(texture.Width), ConvertUnits.ToSimUnits(texture.Height / 2));
+            //_physicsBody.UserData = this;
+            //_physicsBody.CollisionCategories = Category.All;
+            //_physicsBody.CollidesWith = Category.All;
+            //_physicsBody.IgnoreGravity = false;
 
         }
         public void MoveLeft()
         {
-            _Rectangle.X -= 10;
+            _body.Position = new Vector2(_body.Position.X - ConvertUnits.ToSimUnits(5), _body.Position.Y);
         }
 
-
-        public Rectangle Bounds
-        {
-            get
-            {
-                return _Rectangle;
-            }
-        }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_Texture, _physicsBody.Position, Color.White);
+            //spriteBatch.Draw(_Texture, ConvertUnits.ToDisplayUnits(_body.Position), Color.White);
+            spriteBatch.Draw(_Texture, ConvertUnits.ToDisplayUnits(_body.Position), null, Color.White, _body.Rotation, new Vector2(16, 16), 1f, SpriteEffects.None, 0f);
+            
 
         }
         public void MoveRight()
         {
-            _Rectangle.X += 10;
+            _body.Position = new Vector2(_body.Position.X + ConvertUnits.ToSimUnits(5), _body.Position.Y);
         }
         public bool MovingUp()
         {
@@ -60,6 +65,23 @@ namespace FlyingPsychadelia
         {
             return Velocity.X > 0;
             
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                MoveRight();
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                MoveLeft();
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.R))
+            {
+                _body.Position = ConvertUnits.ToSimUnits(new Vector2(0, 0));
+            }
         }
     }
 }
