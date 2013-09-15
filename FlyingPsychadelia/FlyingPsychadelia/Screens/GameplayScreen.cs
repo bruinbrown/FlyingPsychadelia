f9ce7c0f79e57d1b8e28dff88cfa019c061a1fb7
benbradleycom <auto@benbradley.com>
2013-09-15 14:34:37 +0100

    Check End of Level Point from Start Layer

--- FlyingPsychadelia/FlyingPsychadelia/Screens/GameplayScreen.cs
+++ FlyingPsychadelia/FlyingPsychadelia/Screens/GameplayScreen.cs
@@ -88,6 +88,20 @@ namespace FlyingPsychadelia.Screens
 
         #region Update and Draw
 
+        private ObjectLayer GetLayerOrNull(string LayerName)
+        {
+            ObjectLayer Layer = null;
+            try
+            {
+                Layer = _map.ObjectLayers[LayerName];
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Layer;
+        }
+
 
         /// <summary>
         /// Updates the state of the game. This method checks the GameScreen.IsActive
@@ -99,7 +113,8 @@ namespace FlyingPsychadelia.Screens
         {
             base.Update(gameTime, otherScreenHasFocus, false);
 
-            if (_world.Players[0].Bounds.Left > 3150)
+            ObjectLayer StartLayer = GetLayerOrNull("StartLayer");
+            if (_world.Players[0].Bounds.Left > StartLayer.MapObjects[1].Bounds.X )
             {
                 LoadingScreen.Load(ScreenManager, "Yay! Nice Trip!", 0, new MainMenuScreen());
             }
